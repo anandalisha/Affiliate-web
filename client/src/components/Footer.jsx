732eb1bb6483ea76f0817b2d358d89a9a3624263
Alisha Anand <alishaanand86.aa@gmail.com>
2022-05-01 03:35:51 +0530

    src files

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => (
+    <footer className="footer" style={{flexShrink:'0',
+        textAlign:'center',
+        backgroundColor:'black',
+        color:'white'}}>
+      <p>Some footer data here!</p>
+      <h1>Footer</h1>
+    </footer>
+  );
+
+export default Footer;
